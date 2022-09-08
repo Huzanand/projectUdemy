b2f509001c33fca9db176595a84002ef206a71aa
Vald <example@mail.com>
2022-09-09 02:10:42 +0300

    functionExercise3 is done

--- FunctionExercise3.js
+++ FunctionExercise3.js
@@ -14,11 +14,33 @@ function getTimeFromMinutes(value){
 }
 
 function findMaxNumber(a, b, c, d){
-    if(!isNaN(a) && !isNaN(b) && !isNaN(c) && !isNaN(d)){
-        console.log("all is good")
+    if(typeof(a) !== 'number' || typeof(b) !== 'number' || typeof(c) !== 'number' || typeof(d) !== 'number' || a == null || b == null || c == null|| d == null){
+        return 0;
     } else{
-        console.log('0')
+        let aCount = 0, bCount = 0, cCount = 0, dCount = 0;
+        console.log('gg')
+        if(a > b) aCount++;
+        if(a > c) aCount++;
+        if(a > d) aCount++;
+        
+        if(b > a) bCount++;
+        if(b > c) bCount++;
+        if(b > d) bCount++;
+
+        if(c > a) cCount++;
+        if(c > b) cCount++;
+        if(c > d) cCount++;
+
+        if(d > a) dCount++;
+        if(d > b) dCount++;
+        if(d > c) dCount++;
+
+
+        if(aCount > bCount && aCount > cCount && aCount > dCount) return a;
+        if(bCount > aCount && bCount > cCount && bCount > dCount) return b;
+        if(cCount > aCount && cCount > bCount && cCount > dCount) return c;
+        if(dCount > aCount && dCount > bCount && dCount > cCount) return d;
     }
 }
 
-console.log(findMaxNumber(1, 2, 2.5, 'f'))
+console.log(findMaxNumber(1, 5, '6', 11))
