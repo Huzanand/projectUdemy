ffc01c39887189ae691db124cf1615c4173be884
Oleksii Domkin <lehaleha142165@gmail.com>
2022-09-30 05:20:09 +0300

    Rewrite first html project part 1

--- htmlMode/project/js/script.js
+++ htmlMode/project/js/script.js
@@ -10,72 +10,30 @@ document.addEventListener('DOMContentLoaded', () => {
             "Скотт Пилигрим против..."
         ]
     };
-    
+
     const adv = document.querySelectorAll('.promo__adv img'),
-          poster = document.querySelector('.promo__bg'),
-          genre = poster.querySelector('.promo__genre'),
-          movieList = document.querySelector('.promo__interactive-list'),
-          promoIntItem = document.querySelectorAll('.promo__interactive-item');
-    
+          poster = document.querySelector('.promo__bg'), 
+          genre =poster.querySelector('.promo__genre'),
+          movieList = document.querySelector('.promo__interactive-list');
+
     adv.forEach(item => {
         item.remove();
     })
-    
-    genre.textContent = 'Драма';
-    
-    poster.style.backgroundImage = "url('img/bg.jpg')";
-    
-    const sortArray = () =>{
-        movieDB.movies.sort();
-    }
-    
-    const watchFilmLst = function (){
-        movieList.innerHTML = "";
-        movieDB.movies.forEach((film, i) => {
-            movieList.innerHTML += `
-            <li class="promo__interactive-item">${i + 1} ${film}
-                <div class="delete"></div>
-            </li>
-            `;
-        })
-    }
-    sortArray();
-    watchFilmLst();
-    
-    const inputs = document.querySelectorAll('form.add input'),
-          btnConfirm = document.querySelector('form.add button');
-    const trashBox = document.querySelectorAll('.delete');
-    
-    btnConfirm.addEventListener('click', (event) =>{
-        event.preventDefault()
-        let newFilm = inputs[0].value;
-        if(newFilm){
-            if (newFilm.length > 21){
-                newFilm = `${newFilm.slice(-0, 21)}...`
-            }
-            movieDB.movies.push(newFilm);
-            if (inputs[1].checked){
-                console.log('Adding like film')
-            }
-            sortArray();
-            watchFilmLst();
-        }
-    })
-    
-    trashBox.forEach( (item, i) =>{
-        item.addEventListener('click', (event) =>{
-            event.preventDefault();
-            
-            trashBox.paretElement.remove();
-            movieDB.movies.splice(i, 1)
 
-            watchFilmLst();
-            console.log('working')
-    
-            sortArray();
-            watchFilmLst();
-        })
+    genre.textContent = 'драма';
+
+    poster.style.backgroundImage = 'url("img/bg.jpg")';
+
+    movieList.innerHTML = '';
+
+    movieDB.movies.sort();
+
+    movieDB.movies.forEach((film, i) => {
+        movieList.innerHTML += `
+        <li class="promo__interactive-item">${i+1}  ${film}
+            <div class="delete"></div>
+        </li>
+        ` 
     })
-    console.log(movieDB.movies[0])
 })
 
